e061017e4354db2a3f6fe558315ebaa0d2feb471
LuluwahGW <luluwahalgw@gmail.com>
2025-07-23 13:55:36 +0300

    testing

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {"message":"Hello"}
+
+@app.post('/')
+async def post():
+    return{
+        {'message':'hello from this post'}
+    }
+
+
+@app.get('/items')
+async def items():
+    return{
+        {'message':'list of items'}
+    }
