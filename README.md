a8f47277fcaf5e71b16bfcec708b55232fb44814
LuluwahGW <luluwahalgw@gmail.com>
2025-07-24 17:34:51 +0300

    added fastAPI methods + intial readme file

---
+++ README.md
@@ -0,0 +1,18 @@
+# Medication Reminder app
+
+A backend API built with using FastAPI SqlAlchemy for creating and managing users, caregiver, medications and reminder.
+
+# Features
+- user creation and management
+- adding medication (name, dosage, selceted time)
+- reminders (time, reminder text)
+- caregivers linking system
+- 
+## 🛠 Tech Stack
+
+- Python 3.11+
+- FastAPI
+- SQLAlchemy
+- MySQL 
+- Uvicorn
+- Pydantic
--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from enum import Enum
 from pydantic import BaseModel
 import models
 from database import engine, Base , SessionLocal
+from sqlalchemy.orm import Session
 
-from database import SessionLocal
-
-
-print(Base.metadata.tables.keys())
-
-print("Tables registered:", Base.metadata.tables.keys())
 
 
 #creating table based on models
 Base.metadata.create_all(bind=engine)
 
 
-#app = FastAPI()
+app = FastAPI()
 
 
 def create_tables():
@@ -30,11 +25,37 @@ if __name__ == "__main__":
     create_tables()
     
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users")
+def create_user(email: str, password: str, db: Session = Depends(get_db)):
+    existing = db.query(models.User).filter(models.User.email == email).first()
+    if existing:
+        raise HTTPException(status_code=400,detail="Email has already been used")
+
+    new_user = models.User(email=email, password=password)
+    db.add(new_user)
+    db.commit
+    db.refresh(new_user)
+    return{"id ": new_user.id, "email: ":new_user.email}
 
 
-print(Base.metadata.tables.keys())
+@app.get("/users/{user_id}")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id== user_id).first()
+    if not user:
+        raise HTTPException(status_code=404,detail="User not found")
 
+    return{"id": user.id, "email": user.email}
 
-
+@app.get("/users")
+def list_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return[{"id": u.id, "email": u.email}for u in users]    
 
 
